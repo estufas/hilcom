b8601e7133168e3fd98afc93f64d52f13ac5d5ea
Stuart Hiller <hiller.stuart@gmail.com>
2016-06-07 00:09:19 -0700

    recent commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,10 +16,26 @@
 //= require fullpage
 //= require_tree .
 
-$(document).ready(function() {
-			$('#fullpage').fullpage({
-				anchors: ['firstPage', 'secondPage', '3rdPage'],
-				sectionsColor: ['#8FB98B', '#DE564B', '#EAE1C0'],
-				slidesNavigation: true,
-			});
-		});
+$(document).ready(function () {
+    var sildeNum = $('.page').length,
+        wrapperWidth = 100 * sildeNum,
+        slideWidth = 100/sildeNum;
+    $('.wrapper').width(wrapperWidth + '%'); 
+    $('.page').width(slideWidth + '%');
+    $('a.scrollitem').click(function(){
+        $('a.scrollitem').removeClass('selected');
+        $(this).addClass('selected');
+        var slideNumber = $($(this).attr('href')).index('.page'),
+            margin = slideNumber * -100 + '%';
+        $('.wrapper').animate({marginLeft: margin},700);
+        return false;
+    });
+
+    $('.future-background').slick({
+	    dots: true,
+	  	infinite: true,
+	  	speed: 300,
+	  	slidesToShow: 1,
+	  	adaptiveHeight: false
+    });
+});
